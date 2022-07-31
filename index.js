97c3bfdf1185fe8afcde29a631cb8e669bb74914
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-07-31 11:48:12 +0630

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ app.use(express.json())
 
 // morgan(':method :url :status :res[content-length] - :response-time ms')
 
-app.use(morgan('tiny'))
+app.use(morgan('tiny', {
+    skip: (req, res) => req.method !== 'GET'
+}))
+
+morgan.token('req-json', (req, res) => {
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-json', {
+    skip: (req, res) => req.method !== 'POST'
+}))
+
 
 let persons = [
     {
@@ -30,57 +41,57 @@ let persons = [
     }
 ]
 
-app.get('/api/persons',(req,res) => {
+app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
 
-app.post('/api/persons',(req,res) => {
+app.post('/api/persons', (req, res) => {
     const person = {
         id: parseInt(Math.random() * 100000),
         name: req.body.name,
         number: req.body.number,
     }
 
-    if(!person.name){
-        return res.status(400).json({error: `The name is missing`}) 
+    if (!person.name) {
+        return res.status(400).json({ error: `The name is missing` })
     }
-    if(!person.number){
-        return res.status(400).json({error: `The number is missing`}) 
+    if (!person.number) {
+        return res.status(400).json({ error: `The number is missing` })
     }
-    else if(persons.find(p => p.name === person.name)){
-        return res.status(400).json({ error: 'name must be unique' }) 
+    else if (persons.find(p => p.name === person.name)) {
+        return res.status(400).json({ error: 'name must be unique' })
     }
 
-    persons=persons.concat(person)
+    persons = persons.concat(person)
 
     res.json(persons)
 })
 
-app.get('/api/persons/:id',(req,res)=>{
+app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     const person = persons.find(p => p.id === id)
-    
-    if (!person){
+
+    if (!person) {
         return res.status(400).send(`Person with id '${id}' is not in the server.`)
     }
 
     res.json(person)
 })
 
-app.get('/api/persons/delete/:id',(req,res) =>{
+app.get('/api/persons/delete/:id', (req, res) => {
     const id = Number(req.params.id)
     const prevPersons = persons
-    persons= persons.filter(p => p.id !== id)
+    persons = persons.filter(p => p.id !== id)
 
-    if(prevPersons.length === persons.length){
+    if (prevPersons.length === persons.length) {
         return res.send(`Person with id '${id}' is not in the server`)
     }
 
     return res.status(204).end()
 })
 
-app.get('/info',(req,res) => {
+app.get('/info', (req, res) => {
     res.send(`
         <p>Phonebook has info for ${persons.length} people</p>
         <p>${new Date()}</p>
@@ -88,6 +99,6 @@ app.get('/info',(req,res) => {
 })
 
 const PORT = 3001;
-app.listen(PORT,()=> {
+app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
