a2270f1c339be2c0b5d691f054483260ace6c070
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-08-07 21:01:24 +0630

    add front end build folder

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 // morgan(':method :url :status :res[content-length] - :response-time ms')
 
